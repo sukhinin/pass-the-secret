bfaa21f856b8f4be738876f869b7004908c1c9c1
Boris Sukhinin <boris.sukhinin@corp.mail.ru>
2022-01-10 11:41:27 +0300

    improve error handling

--- frontend/version.sh
+++ frontend/version.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 VERSION="$(git describe --dirty --abbrev=8 --always)"
 echo "Version: $VERSION"
 
 for file in "$@"; do
-  if [[ -w "$file" ]]; then
-    echo "Processing file $file"
-    sed -i -e "s/##VERSION##/$VERSION/g" "$file"
-  else
-    echo "File $file is not writeable!"
-    exit 1
-  fi
+  echo "Processing file $file"
+  sed -i -e "s/##VERSION##/$VERSION/g" "$file"
 done
